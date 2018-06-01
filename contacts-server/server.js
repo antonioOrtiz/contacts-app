417f323a6467e3796e0a9f7c86643686603fccbc
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2018-06-01 18:28:32 -0400

    fixed Failed to load resource: the server responded with a status of 403 (Forbidden)

--- contacts-server/server.js
+++ contacts-server/server.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const config = require('./config')
-const contacts = require('./contacts')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const config = require('./config');
+const contacts = require('./contacts');
 
-const app = express()
+const app = express();
 
-app.use(express.static('public'))
-app.use(cors())
+app.use(express.static('public'));
+app.use(cors());
 
 app.get('/', (req, res) => {
   const help = `
@@ -24,44 +24,44 @@ app.get('/', (req, res) => {
     DELETE /contacts/:id
     POST /contacts { name, email, avatarURL }
   </pre>
-  `
+  `;
 
-  res.send(help)
-})
+  res.send(help);
+});
 
 app.use((req, res, next) => {
-  const token = req.get('Authorization')
+  const token = req.get('Authorization');
 
   if (token) {
-    req.token = token
-    next()
+    req.token = token;
+    next();
   } else {
     res.status(403).send({
-      error: 'Please provide an Authorization header to identify yourself (can be whatever you want)'
-    })
+      error: 'Please provide an Authorization header to identify yourself (can be whatever you want)',
+    });
   }
-})
+});
 
 app.get('/contacts', (req, res) => {
-  res.send(contacts.get(req.token))
-})
+  res.send(contacts.get(req.token));
+});
 
 app.delete('/contacts/:id', (req, res) => {
-  res.send(contacts.remove(req.token, req.params.id))
-})
+  res.send(contacts.remove(req.token, req.params.id));
+});
 
 app.post('/contacts', bodyParser.json(), (req, res) => {
-  const { name, email } = req.body
+  const { name, email } = req.body;
 
   if (name && email) {
-    res.send(contacts.add(req.token, req.body))
+    res.send(contacts.add(req.token, req.body));
   } else {
     res.status(403).send({
-      error: 'Please provide both a name and an email address'
-    })
+      error: 'Please provide both a name and an email address',
+    });
   }
-})
+});
 
 app.listen(config.port, () => {
-  console.log('Server listening on port %s, Ctrl+C to stop', config.port)
-})
+  console.log('Server listening on port %s, Ctrl+C to stop', config.port);
+});
