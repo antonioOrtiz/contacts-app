c2838f213842d0e6f65af7749916af74ab9d29da
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2018-06-01 18:29:47 -0400

    constructor method not part of class, as its not needed or being used

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import ImageInput from './ImageInput';
+import serializeForm from 'form-serialize';
 
 export default class CreateContact extends Component {
-  constructor(props) {
-    super(props);
-  }
+  handleSubmit = e => {
+    e.preventDefault();
+    var values = serializeForm(e.target, { hash: true });
 
+    if (this.props.onCreateContact) {
+      this.props.onCreateContact(values);
+    }
+  };
+
+  // prettier-ignore
   render() {
-    return <div>Create Contact</div>;
+    return (
+      <div>
+        <Link className="close-create-contact" to="/">
+          Close
+        </Link>
+        <form onSubmit={this.handleSubmit} className="create-contact-form">
+         <ImageInput
+          className='create-contact-avatar-input'
+          name='avatarURL'
+          maxHeight={64}
+         />
+         <div className="create-contact-details">
+           <input type="text" name="name" placeholder="Name"/>
+           <input type="text" name="email" placeholder="Email"/>
+           <button>Add Contact</button>
+         </div>
+        </form>
+      </div>
+    );
   }
 }
