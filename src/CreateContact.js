64d9dda7c631fdd6910defdfb373fe10daeecdbe
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2018-06-01 16:25:18 -0400

    query property part of state, added sifting method, included PropTypes

---
+++ src/CreateContact.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class CreateContact extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>Create Contact</div>;
+  }
+}
--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,27 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 // prettier-ignore
+export default class ListContacts extends Component {
 
-export default function ListContacts(props){
-  return (
-      <ol className="contact-list">
-        {props.contacts.map(contact => (
-          <li key={contact.id} className="contact-list-item">
-            <div
-              className="contact-avatar"
-              style={{
-                backgroundImage: `url(${contact.avatarURL})`
-              }}>
-            </div>
-            <div className="contact-details">
-              <p>{contact.name}</p>
-              <p>{contact.handle}</p>
-            </div>
-            <button className="contact-remove">Remove</button>
-          </li>))}
-      </ol>
+  static propTypes = {
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact: PropTypes.func.isRequired,
+  }
+
+  state = {
+    query : ''
+  }
+
+  updateQuery = (query) => {
+    this.setState(()=> ({
+      query: query.trim()
+    }))
+  }
+
+  clearQuery = () => {
+    this.updateQuery('')
+  }
+
+  render(){
+    var {query} = this.state
+    var {contacts, onDeleteContact, onNavigate} = this.props
+    var showContacts = query === '' ? contacts : contacts.filter((c)=> (c.name.toLowerCase().includes(query.toLowerCase())))
+    return (
+      <div className="list-contacts">
+        <div className="list-contacts-top">
+          <input
+            type="text"
+            className="search-contacts"
+            placeholder="Search Contacts"
+            value={this.state.query}
+            onChange={(event)=> this.updateQuery(event.target.value)}
+          />
+          <a href="#create" onClick={onNavigate} className="add-contact">Add Contact</a>
+        </div>
+
+        {showContacts.length !== contacts.length && (
+          <div className="showing-contacts">
+            <span>Now showing {showContacts.length} of {contacts.length}</span>
+            <button onClick={this.clearQuery}>Show all</button>
+          </div>
+        )}
+
+        <ol className="contact-list">
+          {showContacts.map(contact => (
+            <li key={contact.id} className="contact-list-item">
+              <div
+                className="contact-avatar"
+                style={{
+                  backgroundImage: `url(${contact.avatarURL})`
+                }}>
+              </div>
+              <div className="contact-details">
+                <p>{contact.name}</p>
+                <p>{contact.email}</p>
+              </div>
+              <button
+              onClick={()=>onDeleteContact(contact)}
+              className="contact-remove">Remove</button>
+            </li>))}
+        </ol>
+      </div>
     );
+  }
 }
-
-
-
