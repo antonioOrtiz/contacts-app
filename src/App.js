99e0641a814b20807714442679a79072a96e49d3
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2018-06-01 16:21:02 -0400

    moved contacts data out of App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
 import React, { Component } from 'react';
+import ListContacts from './ListContacts';
+import * as ContactsAPI from './utils/ContactsAPI';
+import CreateContent from './CreateContact';
 
 class App extends Component {
+  state = {
+    contacts: [],
+    screen: 'list',
+  };
+
+  componentDidMount() {
+    ContactsAPI.getAll().then(contacts => {
+      this.setState(() => ({
+        contacts,
+      }));
+    });
+  }
+
+  removeContact = contact => {
+    this.setState(currentState => ({
+      contacts: currentState.contacts.filter(c => {
+        return c.id !== contact.id;
+      }),
+    }));
+    ContactsAPI.remove(contact);
+  };
+
+  // prettier-ignore
   render() {
     return (
       <div>
-        Hello World
+        {this.state.screen === 'list' && (
+         <ListContacts
+          contacts={this.state.contacts}
+          onDeleteContact={this.removeContact}
+          onNavigate={()=>{
+            this.setState(()=>({
+              screen: 'create'
+            }))
+          }}
+        />
+        )}
+        {this.state.screen === 'create' && (<CreateContent/>)}
       </div>
     );
   }
